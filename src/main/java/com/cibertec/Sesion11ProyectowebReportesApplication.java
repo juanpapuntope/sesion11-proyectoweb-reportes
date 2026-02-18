6051f165dfe29759f4b430c83c0f442dee1b3e72
Juan Pablo <juanpapuntope@gmail.com>
2026-02-18 17:53:25 -0500

    first commit

---
+++ src/main/java/com/cibertec/Sesion11ProyectowebReportesApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sesion11ProyectowebReportesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sesion11ProyectowebReportesApplication.class, args);
+	}
+
+}
